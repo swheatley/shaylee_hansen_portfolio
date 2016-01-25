8020f7e6d79d7dd5d9b48cfca89e9370b2adc21b
swheatley <shaylee1@me.com>
2016-01-25 04:01:09 -0700

    Initial Commit.  Portfolio site ready to go

---
+++ portfolio/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, render_to_response
+from django.conf import settings 
+from django.template import RequestContext
+
+
+def index(request):
+    context = {}
+    return render_to_response('index.html', context, context_instance=RequestContext(request))
+
+
+def about_me(request):
+    context = {}
+    return render_to_response('about-me-index.html', context, context_instance=RequestContext(request))
+
+# Projects
+
+
+def projects(request):
+    context = {}
+    return render_to_response('Projects/projects.html', context, context_instance=RequestContext(request))
+
+
+def google(request):
+    context = {}
+    return render_to_response('Projects/google.html', context, context_instance=RequestContext(request))
+
+
+def google_search(request):
+    context = {}
+    return render_to_response('Projects/google_search.html', context, context_instance=RequestContext(request)) 
+
+
+def luigi_bootstrap(request):
+    context = {}
+    return render_to_response('Projects/luigi.html', context, context_instance=RequestContext(request))
+
+
+def color_grid(request):
+    context = {}
+    return render_to_response('Projects/grid.html', context, context_instance=RequestContext(request))
+
+
+def jquery_fighter(request):
+    context = {}
+    return render_to_response('Projects/j_streetfighter.html', context, context_instance=RequestContext(request))
+
+
+def landing_page(request):
+    context = {}
+    return render_to_response('Projects/landing_page.html', context, context_instance=RequestContext(request))
---
+++ project/urls.py
@@ -0,0 +1,39 @@
+"""portfolio URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static 
+from portfolio import views 
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+
+    url(r'^$', 'portfolio.views.index'),
+    url(r'^about-me-index/$', 'portfolio.views.about_me'),
+
+    # Projects
+
+    url(r'^Projects/projects/$', 'portfolio.views.projects'),
+    url(r'^google/$', 'portfolio.views.google'),
+    url(r'^google_search/$', 'portfolio.views.google_search'),
+
+    url(r'^luigi/$', 'portfolio.views.luigi_bootstrap'),
+    url(r'^grid/$', 'portfolio.views.color_grid'),
+    url(r'^j_streetfighter/$', 'portfolio.views.jquery_fighter'),
+    url(r'^landing_page/$', 'portfolio.views.landing_page'),
+    
+]
